da2fd8ad63c3684c8078c5fc62828b60b53f2189
Camila Rosas <camila.rosas@ucb.edu.bo>
2024-08-26 22:48:57 -0400

    Funcion para todo numero divisible entre 100 pero entre 400 NO es un año bisiestro

--- src/esBisiestro.js
+++ src/esBisiestro.js
@@ -3,10 +3,15 @@ function esBisiestro(anio)
     if(anio % 400 == 0 ){
         return true;
     }else{
-        if(anio === 4)
-            {
-                return true;
-            }
+        if(anio % 100 == 0  && anio % 400 !=0){
+            return false;
+        }
+        else{
+            if(anio === 4)
+                {
+                    return true;
+                }
+        }
     }
     return false;
 }
--- src/esBisiestro.spec.js
+++ src/esBisiestro.spec.js
@@ -2,16 +2,16 @@ import esBisiestro from "./esBisiestro.js";
 
 describe("Es Bisiestro", () => {
   it("El 4 deberia considerarse como año bisiestro", () => {
-    //const result = esBisiestro(4);
     expect(esBisiestro(4)).toEqual(true);
   });
   it("El 5 no deberia considerarse como año bisiestro", () => {
-    //const result = esBisiestro(5);
     expect(esBisiestro(5)).toEqual(false);
   });
   it("Todo año dividido entre 400 es año bisiestro", () => {
-    //const result = esBisiestro(5);
     expect(esBisiestro(2000)).toEqual(true);
+  });
+  it("Todo año dividido entre 100 pero no por 400 NO es año bisiestro", () => {
+    expect(esBisiestro(1700)).toEqual(false);
   });
 });
 
